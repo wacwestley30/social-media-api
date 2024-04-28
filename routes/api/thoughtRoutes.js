63c05c46dbd54f52d2505fe5573eb1cab0868160
wacwestley30 <westleycervantes@gmail.com>
2024-04-28 19:21:43 -0400

    Using thoughtControllers.js as reference filled out the thoughtRoutes.js for each CRUD request and its needed endpoint. Just needs seeds then to test.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(removeReaction);
+
+module.exports = router;
